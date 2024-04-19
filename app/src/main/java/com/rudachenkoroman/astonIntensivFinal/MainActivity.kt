200e58d7b8b0300993acc83a508722a065bdf877
Roman <rudachenko93@mail.ru>
2024-04-19 11:23:33 +0500

    add resources and installSplashScreen

--- app/src/main/java/com/rudachenkoroman/astonIntensivFinal/MainActivity.kt
+++ app/src/main/java/com/rudachenkoroman/astonIntensivFinal/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.rudachenkoroman.astonIntensivFinal
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
+    private var keepSplashOnScreen = true
+    private val delay = 2000L
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        initSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun initSplashScreen(){
+        installSplashScreen().setKeepOnScreenCondition { keepSplashOnScreen }
+        Handler(Looper.getMainLooper()).postDelayed({ keepSplashOnScreen = false }, delay)
     }
 }
